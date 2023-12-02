05fc63ab8e378b63efafbff59e8fa70ee3050654
Nicole Bermudez <60791862+bermudez05@users.noreply.github.com>
2023-12-02 12:48:38 -0500

    format

--- src/components/MenuComponentGerente.jsx
+++ src/components/MenuComponentGerente.jsx
@@ -4,28 +4,28 @@ import {Link} from "react-router-dom";
 function Menu() {
     return (
         <aside className="relative w-1/4 bg-white p-5">
-            <button className="w-full mb-4 p-3 rounded-md bg-color2">
+            <button className="w-full mb-4 p-3 rounded-md bg-color2  hover:bg-gray-400 hover:text-white">
                 <a>
                     <span className="btm-nav-label">Informacion Usuario</span>
                 </a>
             </button>
-            <button className="w-full mb-4 p-3 rounded-md bg-color2">
+            <button className="w-full mb-4 p-3 rounded-md bg-color2 hover:bg-gray-400 hover:text-white">
                 <Link to="productos">
                     <span className="btm-nav-label">Productos existencia</span>
                 </Link>
             </button>
-            <button className="w-full mb-4 p-3 rounded-md bg-color2">
+            <button className="w-full mb-4 p-3 rounded-md bg-color2  hover:bg-gray-400 hover:text-white">
                 <a>
                     <span className="btm-nav-label">Historial transacciones</span>
                 </a>
             </button>
 
             <div className="absolute w-full mb-4 p-3 bottom-0 left-0">
-                <button className="w-full border-1 opacity-70 rounded-md bg-color1 border-gray-500 mb-4 px-5 py-3">
-                    <span className="text-black">Registrar gasto</span>
+                <button className="w-full border-1 rounded-md text-black bg-opacity-60 bg-color1 mb-4 px-5 py-3 hover:bg-color1 hover:text-white">
+                    <span className="btm-nav-label">Registrar gasto</span>
                 </button>
-                <button className="w-full border-1 opacity-70 rounded-md bg-color1 border-gray-500 mb-4 px-5 py-3">
-                    <span className="text-black">Registrar compra</span>
+                <button className="w-full border-1 rounded-md text-black bg-opacity-60 bg-color1 mb-4 px-5 py-3 hover:bg-color1 hover:text-white">
+                    <span className="btm-nav-label">Registrar compra</span>
                 </button>
             </div>
         </aside>
